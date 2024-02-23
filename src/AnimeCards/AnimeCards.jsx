d715c5f0fc571f3bd9ab943371c0fe6a8e9fb729
Yennyferjp <yennyfer.jarava92@gmail.com>
2024-02-23 09:49:29 -0500

    Css de las tarjetas de animes

--- src/AnimeCards/AnimeCards.jsx
+++ src/AnimeCards/AnimeCards.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './AnimeCard.module.css';
 
 function AnimeCard({ anime }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const toggleCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className={style.animeCard}>
-  
-      <img src={anime.images.jpg.image_url}  className={style.animeImage} />
-      {/* <div className={style.animeDetails}> */}
-        <h2 className={style.animeTitle}>{anime.title}</h2>
-        {/* <p className={style.animeSynopsis}>{anime.synopsis}</p>
-        <p className={style.animeScore}>Score: {anime.score}</p> */}
+    <div className={`${style.animeCard} ${isFlipped ? style.flipped : ''}`} onClick={toggleCard}>
+      <div className={style.cardInner}>
+        <div className={style.cardFront}>
+          <img src={anime.images.jpg.image_url} className={style.animeImage} alt={anime.title} title={anime.title} />
+        </div>
+        <div className={style.cardBack}>
+          <p>{anime.title}</p>
+        </div>
       </div>
-    
+      <p className={style.animeRecomendation}>{anime.recommendation}</p> 
+    </div>
   );
 }
 
